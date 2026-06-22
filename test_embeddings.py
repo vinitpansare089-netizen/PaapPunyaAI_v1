c356c8629d5c770277e95973e710a4d9a562be59
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-06-22 23:15:01 +0530

    Testing Embeddings

---
+++ test_embeddings.py
@@ -0,0 +1,16 @@
+"""
+    You: Yaha embedding ki testing kar rahe hai jaise ... 
+         usne teri testing kari thi along with many others vinit...
+
+    Vinit: Personal kyu ho raha hai 
+"""
+
+from services.data_service import load_stories
+from services.embedding_service import EmbeddingService
+
+stories = load_stories("data/krishna_stories.json")
+
+texts = []
+
+for story in stories:
+    texts.append(story['story'])
