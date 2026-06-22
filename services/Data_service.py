595998a7511c7fd7c70ede96db286309772ebb93
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-06-22 20:17:22 +0530

    Adding data service to handle or load stories

--- services/Data_service.py
+++ services/Data_service.py
@@ -1,3 +1,25 @@
+"""
+data_service.py
+
+Purpose:
+Handles loading and management of mythology datasets.
+
+Current Responsibilities:
+- Load Krishna stories from JSON files.
+
+Future Responsibilities:
+- Dataset validation
+- Multi-deity support
+- Database integration
+
+Author:
+Vinit (Trinovous)
+
+Created For:
+PaapPunyaAI_v1
+"""
+
+
 import json
 
 def load_stories(file_path):
@@ -9,5 +31,5 @@ def load_stories(file_path):
         data = json.load(file)
     
     return data['stories']
-    
+
 
