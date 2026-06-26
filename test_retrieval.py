df8024d0b6da6be4a5e2fce2f5176a5989ec36ba
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-06-26 23:08:14 +0530

    stories load ho gai hai sir

--- test_retrieval.py
+++ test_retrieval.py
@@ -2,3 +2,6 @@ from services.data_service import load_stories
 from services.embedding_service import EmbeddingService
 from services.retrieval_service import RetrievalService
 
+##Test krne je liye stories load kar like vo teri krti hai
+stories = load_stories('data/Krishna_stories.json')
+
