66fb719329aa81ad1138563be7e9756aaf104646
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-06-30 15:17:11 +0530

    completed the LLMService

--- services/llm_service.py
+++ services/llm_service.py
@@ -23,12 +23,14 @@ class LLMService:
     def __init__(self, model_name="phi3"):
         self.model_name = model_name
         
-    def generate_responce(self, prompt):
+    def generate_response(self, prompt):
 
-            responce = ollama.chat(
+            response = ollama.chat(
                  model = self.model_name,
                  messages = [{
                       'role': "user",
                       "content": prompt
                  }]
             )
+
+            return response["messages"]["content"]
