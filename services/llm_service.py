310655e98c28487cfd13d0f5d4534a698b4d5b2c
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-06-30 15:14:40 +0530

    Integrated Ollama into LLMService

--- services/llm_service.py
+++ services/llm_service.py
@@ -1,4 +1,34 @@
+"""
+llm_service.py
+
+Purpose:
+Communicate with the LLM through Ollama.
+
+Responsibilities:
+- Send prompt to Ollama
+- Receive generated response
+- Return response
+
+Input:
+- Prompt
+
+Output:
+- LLM Response 
+"""
+import ollama
+
+
 class LLMService:
 
-    def __init__(self):
-        pass
+    def __init__(self, model_name="phi3"):
+        self.model_name = model_name
+        
+    def generate_responce(self, prompt):
+
+            responce = ollama.chat(
+                 model = self.model_name,
+                 messages = [{
+                      'role': "user",
+                      "content": prompt
+                 }]
+            )
