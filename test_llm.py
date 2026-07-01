e2c352a8affdccc7a38e404dd6cf9458b5df1012
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-07-01 16:13:08 +0530

    Testing LLM responses

--- test_llm.py
+++ test_llm.py
@@ -4,10 +4,51 @@ from services.retrieval_service import RetrievalService
 from services.prompt_service import PromptService
 from services.llm_service import LLMService
 
-stories = load_stories('data\Krishna_stories.json')
+#Load the stories
+stories = load_stories('data/Krishna_stories.json')
+# print(stories)
 
-text = []
+##Extract stories in text
+texts = []
 
 for story in stories:
-    text.append(story['story'])
-    
+    texts.append(story['story'])
+
+### Embeddings starts form here
+embeddings = EmbeddingService()
+
+story_embeddings = embeddings.create_embeddings(texts)
+
+###Retrieval starts from here
+retrievalservice = RetrievalService(
+    story_embeddings, stories
+)
+
+###FAISS indexing 
+retrievalservice.create_index()
+
+### ASK Query
+query = "Should I lie to help my friend?"
+
+### Query embedding through embedding service
+query_embeddings = embeddings.create_embeddings([query])
+
+### Retrieve stories
+results = retrievalservice.search(
+    query_embeddings, top_k=3
+)
+
+###Prompt service uses 
+prompt_service = PromptService()
+
+prompt = prompt_service.build_prompt(
+    query,results
+)
+
+llm = LLMService(
+    model_name='phi3'
+)
+
+answer = llm.generate_response(prompt)
+
+print(answer)
