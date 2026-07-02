111f109d166c0327e487fd4a085815f02a08f941
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-07-02 14:38:34 +0530

    FastAPI version 1

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI() 
+app = FastAPI(
+    title="PaapPunyaAI",
+    version="1.0.0",
+    description="AI-powered ethical reasoning system."
+)
 
-@app.get("/")
-def root():
-    return {'message' : 'PaapPunyaAI Is Running.....'}
